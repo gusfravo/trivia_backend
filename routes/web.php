fa322162db7b251804bcc5b1c75f1d12febd30f8
Gustavo Solano <gustavo.esp@icloud.com>
2020-09-25 10:29:45 -0500

    se agrega funcionalidad descargar captura de pantalla

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,46 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/felicitaciones/{id}', function ($id) {
+  $profile = App\Profile::where('user_id',$id)->first();
+  $trivia = App\Trivia::find('1');
+  $game = App\Game::where('profile_id',$profile->id)->where('trivia_id',$trivia->id)->first();
+
+  $listGA = $game->gameAnswers;
+  $listQ = $trivia->questions;
+  $totalCorrect = 0;
+  $totalIncorrect = 0;
+  $totalQuestions =  sizeof($listQ);
+
+  foreach ($listGA as &$item) {
+      if($item->correct == 1){
+        $totalCorrect++;
+      }else{
+        $totalIncorrect++;
+      }
+  }
+  $hour = floor($game->time/1000/60/60);
+  $minute = floor(($game->time/1000/60/60 - $hour)*60);
+  if($profile->img != ''){
+    $img = "server/public/uploads/".$profile->img;
+  }else{
+    $img = "/assets/img/circulo-usuariofinal.png";
+  }
+
+  $requestAux = array(
+    "profile" =>$profile,
+    "trivia" => $trivia,
+    "game" => $game,
+    "score" => [
+      "totalCorrect"=>$totalCorrect,
+      "totalIncorrect"=>$totalIncorrect,
+      "totalQuestions"=>$totalQuestions
+    ],
+    "time" =>[
+      "hour"=>$hour,
+      "minute"=>$minute
+      ],
+    "img" => $img
+  );
+    return view('congrats',['request'=>$requestAux]);
+});
